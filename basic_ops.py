48fc9fc903438ebb55c32189ccbd2c589497ac20
bgrose <bradley@grose.net>
2021-07-28 15:11:34 -0400

    some code i forget what works

--- basic_ops.py
+++ basic_ops.py
@@ -1,6 +1,7 @@
 import cv2
 
 img = cv2.imread('messi5.jpg')
+img2 = cv2.imread('opencv-logo.png')
 
 print(img.shape)
 print(img.size)
@@ -11,6 +12,12 @@ img = cv2.merge((b,g,r))
 ball = img[280:340, 330:390]
 img[273:333, 100:160] = ball
 
-cv2.imshow('image', img)
+img = cv2.resize(img, (512,512))
+img2 = cv2.resize(img2, (512,512))
+
+dst = cv2.add(img, img2);
+
+
+cv2.imshow('image', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
