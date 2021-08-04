70dcbc89472d719a92aa1c78e319428903eca9e6
bgrose <bradley@grose.net>
2021-08-04 15:58:45 -0400

    organization

--- pillow/main.py
+++ pillow/main.py
@@ -1,6 +1,32 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
-im = Image.open("lena.py")
+
+im = Image.open("black-organic-sesame-seeds-isolated-260nw-664802284.jpg")
 im.show()
-im = im.rotate(45)
+im.rotate(45).show()
+
+print(str(im.info))
+
+r, g, b = im.split()
 im.show()
+image = Image.merge("RGB", (b, g, r))
+image.show()
+
+blurImage = im.filter(ImageFilter.BLUR)
+blurImage.show()
+#Save blurImage
+blurImage.save('simBlurImage.jpg')
+
+
+
+#Import all the enhancement filter from pillow
+from PIL.ImageFilter import (
+   BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+   EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN
+)
+#Create image object
+#Applying the blur filter
+img1 = im.filter(CONTOUR)
+img1.save('ImageFilter_blur.jpg')
+img1.show()
+
