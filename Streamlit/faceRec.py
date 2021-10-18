a7334f299a5f51b20623849a07592ea15b9da638
bgrose <bradley@grose.net>
2021-10-18 15:38:44 -0400

    streamlit

--- Streamlit/faceRec.py
+++ Streamlit/faceRec.py
@@ -1,26 +1,34 @@
-import time
 import cv2
 import streamlit as st
 
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
 camera = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('Streamlit\haarcascade_frontalface_default.xml')
+BUTTON_CALCULATE = st.sidebar.button('Calculate')
+WEIGHT = st.sidebar.number_input('Weight', value=0)
 
+i = 0
+calcWait = True
 
-while run:
+while (run):
     _, img = camera.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h),(255, 0, 0) , 3)
     
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.putText(img, "Objects Detected " + str(i), (10,50), font, 1, (0,0, 0), 2, cv2.LINE_AA) 
     FRAME_WINDOW.image(img)
+    i += 1
+    if (BUTTON_CALCULATE):
+        calcWait = False
     
 
 else:
     st.write('Stopped')
+    _, img = camera.read()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.putText(img, "Objects Detected " + str(i), (10,50), font, 1, (0,0, 0), 2, cv2.LINE_AA) 
+    FRAME_WINDOW.image(img)
+    st.sidebar.metric('Weight', i)
